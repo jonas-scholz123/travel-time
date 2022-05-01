78c0847179c4bc45eb6a25d7e8c50cbcc5270634
Jonas Scholz <jonas.scholz123@gmail.com>
2022-05-01 10:31:05 +0100

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use api::{
         stops_response::{StopPointMode, StopsResponse},
     },
 };
-use serde_json::to_string;
 
 mod api;
 pub mod utils;
@@ -16,21 +15,6 @@ async fn main() {
     let client = TFLClient::new("7fa56d767da04461a225dfe82d34ef51").unwrap();
     let request = StopsByModeRequest::new(vec![StopPointMode::Dlr]);
     let response: Result<StopsResponse> = client.query(&request).await;
-
-    let b = vec![StopPointMode::Dlr, StopPointMode::Tube];
-    let a = b
-        .iter()
-        .map(|c| {
-            // Parsed as json string ""enum"". Remove first and last.
-            let string = to_string(c).unwrap();
-            let mut chars = string.chars();
-            chars.next();
-            chars.next_back();
-            chars.as_str().to_string()
-        })
-        .collect::<Vec<String>>()
-        .join(",");
-    println!("{}", a);
 
     match response {
         Ok(_) => {
