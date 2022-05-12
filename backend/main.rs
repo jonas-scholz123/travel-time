428d00d0fb5dbe39a276450f26f7bbe9827bacc9
Jonas Scholz <jonas.scholz123@gmail.com>
2022-05-12 23:48:45 +0100

    Fix clap help text

--- backend/main.rs
+++ backend/main.rs
@@ -10,39 +10,39 @@ mod utils;
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 pub struct SetupArgs {
-    // Should stop points be loaded from the TFL api?
+    /// Should stop points be loaded from the TFL api?
     #[clap(short, long)]
     stoppoint_load: bool,
-    // Should routes be loaded from the TFL api?
+    /// Should routes be loaded from the TFL api?
     #[clap(short, long)]
     routes_load: bool,
-    // Should route segments (i.e. between individual stops)
-    // be loaded from the TFL api?
+    /// Should route segments (i.e. between individual stops)
+    /// be loaded from the TFL api?
     #[clap(short, long)]
     segment_load: bool,
-    // Should timetables (departure times) be loaded from the TFL api?
+    /// Should timetables (departure times) be loaded from the TFL api?
     #[clap(short, long)]
     timetable_load: bool,
-    // Should existing timetable data in Mongo be fixed?
-    // This involves sorting the departure times.
+    /// Should existing timetable data in Mongo be fixed?
+    /// This involves sorting the departure times.
     #[clap(long)]
     fix_timetables: bool,
-    // Should existing stop point data in Mongo be fixed?
-    // This involves adding a (guessed) TIPLOC ID to every entry.
+    /// Should existing stop point data in Mongo be fixed?
+    /// This involves adding a (guessed) TIPLOC ID to every entry.
     #[clap(long)]
     fix_stoppoints: bool,
-    // Should we download the zip file containing national rail
-    // timetables from S3?
+    /// Should we download the zip file containing national rail
+    /// timetables from S3?
     #[clap(long)]
     load_national_rail_data: bool,
-    // Should national rail timetable data be inserted into Mongo?
+    /// Should national rail timetable data be inserted into Mongo?
     #[clap(short, long)]
     load_national_rail: bool,
-    // Should the TFLGraph be constructed locally?
+    /// Should the TFLGraph be constructed locally?
     #[clap(long)]
     build_graph: bool,
-    // Should data be copied from the local MongoDB to
-    // the hosted one?
+    /// Should data be copied from the local MongoDB to
+    /// the hosted one?
     #[clap(short, long)]
     copy_to_atlas: bool,
 }
