b9134143f8bfa75475e88fb2c07022cc83cbe84f
Jonas Scholz <jonas.scholz123@gmail.com>
2024-01-20 15:03:35 +0000

    wip2

---
+++ frontend/src/components/MapClickHandler.jsx
@@ -0,0 +1,13 @@
+import { useMapEvents } from 'react-leaflet';
+
+const MapClickHandler = ({ onClick }) => {
+    useMapEvents({
+        click: (e) => {
+            onClick(e);
+        },
+    });
+
+    return null;
+};
+
+export default MapClickHandler;
